3cd246f99fae8debd2e27d5222b6c012163312d2
John Oblenda <joandong2@gmail.com>
2022-10-02 13:32:10 -0400

    boilerplate

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // colors: {
+      //   lightBlack: '#5c5c5c',
+      // },
+    },
+    fontFamily: {
+      'oswald': ['Oswald', 'sans-serif'],
+      'istok': ['Istok Web', 'sans-serif']
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1140px',
+    },
+  },
+  plugins: [],
+}
